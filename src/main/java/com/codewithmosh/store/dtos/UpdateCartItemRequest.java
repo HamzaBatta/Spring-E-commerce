5dd14cf7e8ec48efaf28ddc38c84f20566f42977
HamzaTB202 <hamzatb202@gmail.com>
2026-05-01 15:14:02 +0300

    first commit (e-commerce app)

---
+++ src/main/java/com/codewithmosh/store/dtos/UpdateCartItemRequest.java
@@ -0,0 +1,15 @@
+package com.codewithmosh.store.dtos;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class UpdateCartItemRequest {
+    @NotNull(message = "Quantity is required")
+    @Min(value = 1, message = "Quantity must be at least 1")
+    @Max(value = 1000, message = "Quantity must be at most 10")
+    private Integer quantity;
+
+}
